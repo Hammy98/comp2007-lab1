35033d4d0c890200f5a9938e26baace9d53e88f6
Harman474 <harmandeepsingh474@gmail.com>
2018-02-13 22:57:57 -0500

    Lab1

---
+++ Lab1/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //Get:Product/Details
+        public ActionResult Details(string product)
+        {
+            //check  empty Product
+            if (product == null)
+            {
+                return View("Error");
+            }
+            ViewBag.Product = product;
+            return View();  
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+<h1>COMP2007 - Lab1</h1>
+
+<p>This is ASP.NET Lab1 Created by Using the Concepts done in Class.</p>
+
+<p>The demo site can be found at <a href="https://comp2007-pm-musicstore.azurewebsites.net/">https://comp2007-pm-musicstore.azurewebsites.net/</a>.</p>
